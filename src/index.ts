b2f4df38414f09e7dae1eae01d06b3e25ad4d579
Gary Hsiao <garyh6@users.noreply.github.com>
2019-12-28 14:40:22 -0800

    added test db, extract start server logic, add test case for registration

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
-import { importSchema } from "graphql-import";
-import { GraphQLServer } from "graphql-yoga";
-import * as path from "path";
 import "reflect-metadata";
-import { createConnection } from "typeorm";
-import { resolvers } from "./resolvers";
+import { startServer } from "./startServer";
 
-const typeDefs = importSchema(path.join(__dirname, "/schema.graphql"));
-
-const server = new GraphQLServer({ typeDefs, resolvers });
-createConnection().then(() => {
-  server.start(() => console.log("Server is running on localhost:4000"));
-});
+startServer();
---
+++ src/tests/register.test.ts
@@ -0,0 +1,39 @@
+import { request } from "graphql-request";
+import { User } from "../entity/User";
+import { startServer } from "../startServer";
+
+let getHost = () => "";
+interface AddressInfo {
+  address: string;
+  family: string;
+  port: number;
+}
+beforeAll(async () => {
+  const app = await startServer();
+  const { port } = app.address() as AddressInfo;
+  getHost = () => `http://127.0.0.1:${port}`;
+});
+
+const email = "gary1@test.com";
+const password = "test";
+
+const mutation = `
+mutation {
+    register(email: "${email}", password:"${password}")
+}`;
+
+test("Register user", async () => {
+  const response = await request(getHost(), mutation);
+  expect(response).toEqual({ register: true });
+
+  const users = await User.find({ where: { email } });
+  expect(users).toHaveLength(1);
+  const user = users[0];
+  expect(user.email).toEqual(email);
+  expect(user.password).not.toEqual(password);
+});
+// going through Server, going through graphql engine
+
+// use test db
+// drop all data once test is over
+// when i run yarn test it also starts the server
