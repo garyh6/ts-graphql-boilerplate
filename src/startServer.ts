4d2b0b1f5d8dc1612a1801a19ac564eeaf941789
Gary Hsiao <garyh6@users.noreply.github.com>
2019-12-29 19:08:03 -0800

    fix gen-schema-types

--- src/startServer.ts
+++ src/startServer.ts
@@ -1,26 +1,12 @@
-import * as fs from "fs";
-import { GraphQLSchema } from "graphql";
-import { importSchema } from "graphql-import";
-import { makeExecutableSchema, mergeSchemas } from "graphql-tools";
 import { GraphQLServer } from "graphql-yoga";
-import * as path from "path";
 import { redis } from "./redis";
 import { confirmEmail } from "./routes/confirmEmail";
 import { createTypeormConn } from "./utils/createTypeormConn";
+import { genSchema } from "./utils/generateSchema";
 
 export const startServer = async () => {
-  const schemas: GraphQLSchema[] = [];
-  const folders = fs.readdirSync(path.join(__dirname, "./modules"));
-  folders.forEach(folder => {
-    const { resolvers } = require(`./modules/${folder}/resolvers`);
-    const typeDefs = importSchema(
-      path.join(__dirname, `./modules/${folder}/schema.graphql`)
-    );
-    schemas.push(makeExecutableSchema({ resolvers, typeDefs }));
-  });
-
   const server = new GraphQLServer({
-    schema: mergeSchemas({ schemas }),
+    schema: genSchema(),
     context: ({ request }) => ({
       redis,
       url: request.protocol + "://" + request.get("host")
